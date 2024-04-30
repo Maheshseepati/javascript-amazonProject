692a36575d8d77f1921bdd84161a1e5e1bb3b6f1
Maheshseepati <maheshseepati@gmail.com>
2024-04-30 12:03:13 +0530

    use modules import and export

--- javascript-amazon-project-main/script/amazon.js
+++ javascript-amazon-project-main/script/amazon.js
@@ -1,98 +1,120 @@
-let productHTML = '';
-products.forEach((product)=>{
-    productHTML +=`
-    <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
+  import { cart as mycart } from "../data/cart.js";
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+  let productHTML = '';
+  products.forEach((product)=>{
+      productHTML +=`
+      <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.image}">
             </div>
-          </div>
 
-          <div class="product-price">
-            $${(product.priceCents/100).toFixed(2)}
-          </div>
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
 
-          <div class="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
 
-          <div class="product-spacer"></div>
+            <div class="product-price">
+              $${(product.priceCents/100).toFixed(2)}
+            </div>
 
-          <div class="added-to-cart ">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
+            <div class="product-quantity-container">
+              <select class ="js-quantity-selector-${product.id}">
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-            Add to Cart
-          </button>
-        </div>`
+            <div class="product-spacer"></div>
 
-        
-        
-});
-//console.log(productHTML);
-document.querySelector('.js-products-grid-button').innerHTML = productHTML;
+            <div class="added-to-cart js-added-to-cart-${product.id} ">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
 
-document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+              Add to Cart
+            </button>
+          </div>`
 
-  button.addEventListener('click',()=>{
+          
+          
+  });
+  //console.log(productHTML);
+  document.querySelector('.js-products-grid-button').innerHTML = productHTML;
 
-    const productId = button.dataset.productId;
+  const addedMessageTimeout = {};
 
-    let matchItem = '';
+  document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
 
-    cart.forEach((item)=>{
-      if(productId === item.productId){
+    button.addEventListener('click',()=>{
 
-        matchItem = item;
+      const {productId} = button.dataset;
 
-      }
-    });
-    if(matchItem){
-      matchItem.quantity +=1;
-    }else{
-      cart.push({
-        productId:productId,
-        quantity:1
+      let matchItem = '';
+
+      cart.forEach((item)=>{
+        if(productId === item.productId){
+
+          matchItem = item;
+
+        }
       });
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+      const quantity =Number( quantitySelector.value);
+      console.log(quantity);
+      if(matchItem){
+        matchItem.quantity += quantity;
+      }else{
+        cart.push({
+          // productId:productId,
+          // quantity:quantity
+          productId,
+          quantity
 
-    };
-    cartQuantity =0;
+        });
 
-    cart.forEach((item)=>{
-      cartQuantity += item.quantity;
-    });
-    console.log(cartQuantity);
+      };
+      cartQuantity = 0;
 
-    document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+      });
+  document.querySelector('.cart-quantity').innerHTML = cartQuantity;
 
-  
+  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+  addedMessage.classList.add('added-to-cart-visible');
+  const previoustimeoutId = addedMessageTimeout[productId];
+  if(previoustimeoutId){
+    clearTimeout(previoustimeoutId);
+  }
+
+ const timeoutId= setTimeout(() => {
+    addedMessage.classList.remove('added-to-cart-visible');
+  }, 2000);
+
+  addedMessageTimeout[productId] = timeoutId;
 
     
 
-  });
+      
 
-});
+    });
+
+  });
