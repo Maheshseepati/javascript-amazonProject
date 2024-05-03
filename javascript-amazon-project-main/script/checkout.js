4540cfe550bf7e7e754666006b92a6f83ddcc794
Maheshseepati <maheshseepati@gmail.com>
2024-05-03 14:00:10 +0530

    delete products from html when clicking delete

--- javascript-amazon-project-main/script/checkout.js
+++ javascript-amazon-project-main/script/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 let cartSummaryHtml='';
@@ -12,7 +12,7 @@ cart.forEach((cartItem)=>{
         }
     });
     cartSummaryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -30,12 +30,12 @@ cart.forEach((cartItem)=>{
         </div>
         <div class="product-quantity">
           <span>
-            Quantity: <span class="quantity-label">${cartItem.quantity}/span>
+            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
           </span>
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -92,3 +92,13 @@ cart.forEach((cartItem)=>{
 
 });
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
+
+document.querySelectorAll('.js-delete-link').forEach((link)=>{
+  link.addEventListener('click',()=>{
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+
+   const container= document.querySelector(`.js-cart-item-container-${productId}`);
+   container.remove();
+  });
+})
